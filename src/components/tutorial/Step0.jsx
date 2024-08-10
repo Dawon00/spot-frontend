1a878149b12478fb2435d086d4a98439bdfa7348
Dawon00 <dawon000203@gmail.com>
2024-08-11 08:25:14 +0900

    fix: logo 위치 변경

--- src/components/tutorial/Step0.jsx
+++ src/components/tutorial/Step0.jsx
@@ -4,14 +4,10 @@ import posco from "../../assets/posco.svg";
 
 function Step0() {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center bg-[#151515]">
-      <div className="flex justify-center items-center mt-60">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-[#151515]">
+      <div className="flex justify-center items-center">
         <img src={Logo} alt="logo" className="w-[124px]" />
       </div>{" "}
-      <div className="flex justify-center items-center mt-60">
-        <img src={posco} alt="posco" className="mr-3" />
-        <img src={pohang} alt="pohang" />
-      </div>
     </div>
   );
 }
