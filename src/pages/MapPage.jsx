5a449cafccad1627ce40d2e4552f9beb0b2d3192
Dawon00 <dawon000203@gmail.com>
2024-08-10 21:44:53 +0900

    feat: LocationSelector 경로 탐색 상태일 때의 버전 구현

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,18 +1,16 @@
-
-import React, { useState } from 'react';
-import TmapComponent from '../components/TmapComponent';
-import { useForm } from 'react-hook-form';
-import axios from 'axios';
+import React, { useState } from "react";
+import TmapComponent from "../components/TmapComponent";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import LocationSelector from "../components/LocationSelector";
 
 const MapPage = () => {
-
-
-    return (
-        <div className='min-h-screen'>
-            <TmapComponent />
-        </div>
-
-    );
+  return (
+    <div className="min-h-screen">
+      {/* <TmapComponent /> */}
+      <LocationSelector />
+    </div>
+  );
 };
 
 export default MapPage;
