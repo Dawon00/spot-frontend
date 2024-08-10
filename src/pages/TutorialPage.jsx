bf0f520151fba76a871b757bbef9964e9abe6e5f
Dawon00 <dawon000203@gmail.com>
2024-08-11 00:11:49 +0900

    chore: import 순서 변경

--- src/pages/TutorialPage.jsx
+++ src/pages/TutorialPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import WideButton from "../components/WideButton";
 import { currentStepState } from "../atom/tutorialState";
 import { useRecoilState } from "recoil";
+import Step0 from "../components/tutorial/Step0";
 import Step1 from "../components/tutorial/Step1";
 import Step2 from "../components/tutorial/Step2";
 import Step3 from "../components/tutorial/Step3";
@@ -11,7 +12,6 @@ import Step6 from "../components/tutorial/Step6";
 import Step7 from "../components/tutorial/Step7";
 import Step8 from "../components/tutorial/Step8";
 import Step9 from "../components/tutorial/Step9";
-import Step0 from "../components/tutorial/Step0";
 
 function TutorialPage() {
   const [currentStep, setCurrentStep] = useRecoilState(currentStepState);
@@ -43,10 +43,19 @@ function TutorialPage() {
       : "다음";
 
   return (
-    <div className="w-full min-h-screen bg-black">
-      {steps[currentStep]}
+    <div className="w-full min-h-screen bg-black relative overflow-hidden">
+      {steps.map((step, index) => (
+        <div
+          key={index}
+          className={`absolute inset-0 transition-opacity duration-300 ease-in-out ${
+            currentStep === index ? "opacity-100 z-10" : "opacity-0 z-0"
+          }`}
+        >
+          {step}
+        </div>
+      ))}
       {currentStep !== 6 && currentStep < steps.length - 1 && (
-        <div className="absolute bottom-0 w-full flex justify-center p-4">
+        <div className="absolute bottom-0 w-full flex justify-center p-4 z-20">
           <WideButton onClick={handleNext}>{buttonText}</WideButton>
         </div>
       )}
