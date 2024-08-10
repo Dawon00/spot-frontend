8bf16c698e247d0e1233bdf0d5a98f031d3840bf
Dawon00 <dawon000203@gmail.com>
2024-08-11 05:09:08 +0900

    refactor: svg 이미지 분리

---
+++ src/assets/CurrentMarker.js
@@ -0,0 +1,23 @@
+export const CurrentMarker = `
+<svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="18" cy="18" r="17" fill="#00FFAE" fill-opacity="0.5"/>
+<g filter="url(#filter0_d_184_4602)">
+<circle cx="18" cy="18" r="8" fill="#009DFF"/>
+<circle cx="18" cy="18" r="6.5" stroke="black" stroke-width="3"/>
+</g>
+<defs>
+<filter id="filter0_d_184_4602" x="0" y="0" width="36" height="36" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset/>
+<feGaussianBlur stdDeviation="5"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.615686 0 0 0 0 1 0 0 0 1 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_184_4602"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_184_4602" result="shape"/>
+</filter>
+</defs>
+</svg>
+
+
+`;
